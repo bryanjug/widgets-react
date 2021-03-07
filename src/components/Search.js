b450deac63d6caa09b701da1c48d3daccbe523cc
bryanjug <bryandjug@gmail.com>
2021-03-06 18:25:16 -0800

    added search engine for wikipedia

---
+++ src/components/Search.js
@@ -0,0 +1,63 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const Search = () => {
+    const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        const search = async () => {
+            const response = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                },
+            });
+            
+            setResults(response.data.query.search);
+        };
+
+        if(term) { 
+            search();
+        }
+    }, [term]);
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div className="item" key={result.pageid}>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{__html: result.snippet}}>
+                    
+                    </span>
+                </div>
+            </div>
+        );
+    });
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input 
+                    type="text" 
+                    className="input"
+                    value={term}
+                    onChange={e => setTerm(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
+            </div>
+        </div>
+    );
+};
+
+export default Search;
