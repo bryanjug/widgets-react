0e1837481d350305716bf6395ac4dd02563b9353
bryanjug <bryandjug@gmail.com>
2021-03-06 20:57:11 -0800

    added debouncedTerm for search engine

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,8 +2,19 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 500);
+
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [term]);
 
     useEffect(() => {
         const search = async () => {
@@ -13,21 +24,27 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term,
+                    srsearch: debouncedTerm,
                 },
             });
             
             setResults(response.data.query.search);
         };
 
-        if(term) { 
-            search();
-        }
-    }, [term]);
+        search();
+    }, [debouncedTerm]);
 
     const renderedResults = results.map((result) => {
         return (
             <div className="item" key={result.pageid}>
+                <div className="right floated content">
+                    <a 
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                        className="ui button"
+                    >
+                        Go
+                    </a>
+                </div>
                 <div className="content">
                     <div className="header">
                         {result.title}
