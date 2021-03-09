95b8405aab000a1e0acc900265b6652a6d710755
bryanjug <bryandjug@gmail.com>
2021-03-08 21:33:25 -0800

    added event listeners and finished project

--- src/App.js
+++ src/App.js
@@ -35,10 +35,15 @@ const options = [
 
 export default () => {
     const [selected, setSelected] = useState(options[0]);
+    const [showDropdown, setShowDropdown] = useState(true);
 
     return (
         <div>
-            <Dropdown options={options} selected={selected} onSelectedChange={setSelected} />
+            <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Drop Down</button>
+            {showDropdown ?
+                <Dropdown options={options} selected={selected} onSelectedChange={setSelected} /> 
+                : null
+            }
         </div>
     );
 };
