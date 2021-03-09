d7f4aa3ca08b4728f3fcc800dc7bc13f6dbbae38
bryanjug <bryandjug@gmail.com>
2021-03-09 01:52:47 -0800

    created translate components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import ColoredText from './components/ColoredText';
+import Translate from './components/Translate';
 
 const items = [
     {
@@ -35,19 +36,9 @@ const options = [
 ];
 
 export default () => {
-    const [selected, setSelected] = useState(options[0]);
-    const [showDropdown, setShowDropdown] = useState(true);
-
     return (
         <div>
-            <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Drop Down</button>
-            {showDropdown ?
-                <div>
-                    <Dropdown options={options} selected={selected} onSelectedChange={setSelected} /> 
-                    <ColoredText selected={selected}/>
-                </div>
-                : null
-            }
+            <Translate />
         </div>
     );
 };
